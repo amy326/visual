63b1a4612c11fc5d2279d698e07248a749b1ba62
yyz1124 <amy87326@163.com>
2018-02-24 23:36:35 +0800

    add method.cs

---
+++ method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace hello
+{
+      public class Music
+      {
+            public string Title { get; set; } //有 get 和 set 就表示这个 Title 是属性
+            public int Year { get; set; }
+            public void Play()
+            {
+                  // 这里 Play 表示的是方法，这部分放方法的具体内容。播放音乐是一种发放，void 表示不需要回传值。如果需要，可以加 return
+                  int Add(int a; int b;)
+                  {
+                        return a + b;
+                  }
+            }
+      }
+}
